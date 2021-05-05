017fc8fa1c90bc0728bcf67d8b6e0abeadb61624
Jônata Marcelino <jonata.marcelino83@gmail.com>
2021-05-05 18:33:11 -0300

    Paginação

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -15,17 +15,20 @@ public class SaleDTO implements Serializable {
 	private Double amount;
 	private LocalDate date;
 	
+	private SellerDTO seller;
+	
 	public SaleDTO() {
 		
 	}
 
-	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date) {
+	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO seller) {
 		super();
 		this.id = id;
 		this.visited = visited;
 		this.deals = deals;
 		this.amount = amount;
 		this.date = date;
+		this.seller = seller;
 	}
 
 	public SaleDTO(Sale entity) {
@@ -34,6 +37,7 @@ public class SaleDTO implements Serializable {
 		deals = entity.getDeals();
 		amount = entity.getAmount();
 		date = entity.getDate();
+		seller = new SellerDTO(entity.getSeller());
 	}
 
 	public Long getId() {
@@ -75,7 +79,13 @@ public class SaleDTO implements Serializable {
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
-	
-	
+
+	public SellerDTO getSeller() {
+		return seller;
+	}
+
+	public void setSeller(SellerDTO seller) {
+		this.seller = seller;
+	}
 
 }
--- backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -1,14 +1,15 @@
 package com.devsuperior.dsvendas.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -16,9 +17,17 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<SaleDTO> findAll() {
-		List<Sale> result = repository.findAll();
-		return result.stream().map(x -> new SaleDTO(x)).collect(Collectors.toList());
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		
+		// Guarda os vendedores na memória para evitar buscas recursivas, caso o tamanho for maior não tem problema
+		sellerRepository.findAll();
+		
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
 				
 	}
 	
